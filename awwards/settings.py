23a7298f5359bc1364bcc33ebd05578bd9b80a12
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-06-11 21:14:24 +0300

    st:add profile image on profile page

--- awwards/settings.py
+++ awwards/settings.py
@@ -122,11 +122,16 @@ MEDIA_URL='media/'
 
 MEDIA_ROOT= BASE_DIR/ 'media/'
 
-STATIC_DIRS=[
+STATICFILES_DIRS=[
     BASE_DIR / 'media/'
 ]
 
 STATIC_ROOT=BASE_DIR/ 'staticfiles'
+
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
