f704187742deae03704ef96abc7b4addef4680b2
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-06-11 20:11:23 +0300

    bug:fix user relationship with profile relationship

--- api/models.py
+++ api/models.py
@@ -1,13 +1,10 @@
-from distutils.command import upload
-from email.policy import default
-from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Profile(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
     bio=models.CharField(max_length=200, blank=True, null=True)
     contact=models.CharField(max_length=100, blank=True, null=True)
     image=models.ImageField(default='default.png', upload_to='images')
