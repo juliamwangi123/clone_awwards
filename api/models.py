a1155786358a6a39cf57c3109999fae3b1c8fcbc
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-06-11 19:52:29 +0300

    rf:remove contact attribute from profile model

--- api/models.py
+++ api/models.py
@@ -10,7 +10,6 @@ class Profile(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     bio=models.CharField(max_length=200, blank=True, null=True)
     contact=models.CharField(max_length=100, blank=True, null=True)
-    country=models.CharField(max_length=200, blank=True, null=True)
     image=models.ImageField(default='default.png', upload_to='images')
 
 
