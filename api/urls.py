2ad7faec9cda66ba8e870a7326c83bd78f58696f
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-06-11 19:34:06 +0300

    ft:add profile view and render it to a template

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     path('' , views.regUser,  name='api-reg'),
     path('home' ,views.home , name='home'),
-    path('login' , views.loginUser, name='login')
+    path('login' , views.loginUser, name='login'),
+    path('profile', views.userProfile, name='profile')
 ]
