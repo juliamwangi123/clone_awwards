1e0bda776d8a5abf07506d436dac68ce1eca855f
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-06-11 18:29:21 +0300

    ft:add user profile model

--- api/models.py
+++ api/models.py
@@ -2,3 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user=models.ForeignKey(User)
+    bio=models.CharField(max_length=200, blank=True, null=True)
+    contact=models.CharField(max_length=100, blank=True, null=True)
+    country=models.CharField(max_length=200, blank=True, null=True)
